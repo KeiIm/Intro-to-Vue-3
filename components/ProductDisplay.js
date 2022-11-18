90f602fd89447414fdbb427eb41c60eea8809eae
Kei <marios_bestfriend@htmail.com>
2022-11-18 14:11:30 -0500

    Add Components

---
+++ components/ProductDisplay.js
@@ -0,0 +1,94 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:
+        // Below triggers es6-string-html
+        /*html*/
+        `<div class="product-display">
+            <div class="product-container">
+
+            <div class="product-image">
+                <img :src="image" :class="{'out-of-stock-img': !inventory}">
+            </div>
+            <!-- :src is short for v-bind:src -->
+
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+
+                <product-details :details="details"></product-details>
+
+                <div v-for="(variant,index) in variants" :key="variant.id" @mouseover="updateVariant(index)"
+                class="color-circle" :style="{backgroundColor: variant.color}">
+                </div>
+
+                <div v-for="(size, index) in sizes" :key="index">{{size}}</div>
+
+                <p v-if="inventory > 2">In Stock!</p>
+                <p v-else-if="inventory <= 2 && inventory > 0" style="color:darkorange">Few Remaining!</p>
+                <p v-else style="color:red">Out of Stock</p>
+                <p><b>{{onSale}}</b></p>
+                <!--Earlier: v-show="onSale"-->
+
+                <p>Shipping: {{shipping}}</p>
+
+                <button class="button" :class="{disabledButton: !inventory}" :disabled="!inventory" @click="addToCart">
+                Add to Cart</button>
+                <button class="button" @click="removeFromCart">Remove Item</button>
+                <!-- "@" replaces "v-on:" -->
+            </div>
+            </div>
+        </div>`,
+
+    data() {                //ES6 Shorthand for data: () => {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0, //0 is green, 1 is blue
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', inventory: 5, onSale: true },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', inventory: 0, onSale: false }
+            ],
+            sizes: ['S', 'M', 'L', 'XL']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+            this.inventory() > 0 ? this.variants[selectedVariant].inventory -= 1 : '';
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+                this.variants[selectedVariant].inventory += 1;
+            }
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        onSale() {
+            if (this.variants[this.selectedVariant].onSale) {
+                return `${this.brand} ${this.product} is on sale!`
+            } else return '';
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inventory() {
+            return this.variants[this.selectedVariant].inventory
+        },
+        shipping() {
+            return this.premium ? "Free" : "2.99"
+        }
+    }
+})
+
--- main.js
+++ main.js
@@ -4,38 +4,10 @@ const app = Vue.createApp({
     data() {                //ES6 Shorthand for data: () => {
         return {
             cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://static.nrdbassets.com/v1/large/30001.jpg',
-            inventory: 10,
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
-            ],
-            sizes: ['S', 'M', 'L', 'XL']
+            premium: true,
+            url: 'https://static.nrdbassets.com/v1/large/30001.jpg'
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1;
-            this.inventory > 0 ? this.inventory -= 1 : this.inventory = 0
-        },
-        removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1
-                this.inventory += 1;
-            }
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        }
-    }
-})
+    methods: {}
+}
+)
